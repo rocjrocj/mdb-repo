ec0a4dd23673700fe5feb5666986b79ca054a792
rocjrocj <roc@rocjohnson.com>
2016-12-09 20:35:34 -0600

    com2

---
+++ sidenav.php
@@ -0,0 +1,15 @@
+<!--Side Navigation-->
+<sidenav>
+	<ul>
+		<!--Navbar Brand-->
+		<li><a class="navbar-brand flex-center" href="index.html"><img src="img/A-220x228.png" alt="Alegion branding" width="26" height="27" class=""> <span class="sr-only">(current)</span></a></li>
+		<li>
+			<div id="workflows" class="sidenav-div bt-blu1 bb-blu1"><a href=""><i class="fa fa-cogs flex-center white-text fa-lg" aria-hidden="true"></i></a>
+				<div id="workflows-hover" class="sidenav-hover z-depth-1-half">Workflows</div>
+			</div>
+		</li>
+		<li class="bb-blu1"><a href=""><i class="fa fa-code flex-center white-text fa-lg" aria-hidden="true"></i></a></li>
+		<li class="bb-blu1"><a href=""><i class="fa fa-question-circle flex-center white-text fa-lg" aria-hidden="true"></i></a></li>
+	</ul>
+</sidenav>
+<!--/Side Navigation--> 
